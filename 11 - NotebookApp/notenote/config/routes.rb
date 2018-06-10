070bfc2b742f0814741f88daf648999086a46076
Sharon Vaughan <sharon.vaughan@envato.com>
2018-06-10 13:26:46 +1000

    Change root based on whether user_signed_in?

--- 11 - NotebookApp/notenote/config/routes.rb
+++ 11 - NotebookApp/notenote/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
+  resources :notes
+
+  authenticated :user do
+    root 'notes#index', as: 'authenticated_root'
+    # root 'notes#index'  -------this still works, probs rails 5 or devise upgrade
+  end
 
   root 'welcome#index'
-
-  resources :notes
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
